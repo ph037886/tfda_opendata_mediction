0412deb1e1b8828d2772f5affce7103bd6dfce22
duncanfun <u101530009@gap.kmu.edu.tw>
2025-09-03 18:35:46 +0800

    更新： 1. 健保資料庫產生許可證字號，由健保碼轉換，改為由藥品連結轉換

--- update_database.py
+++ update_database.py
@@ -68,27 +68,19 @@ def download_new_source_and_update():
     temp_zip.close()
     os.remove('files/from_tfda/temp.zip')
 
-def update_nhi_database():
-    url='https://data.nhi.gov.tw/Datasets/DatasetResource.ashx?rId=A21030000I-E41001-001'
-    response = requests.get(url)
-    open('files/from_tfda/健保用藥品項查詢項目檔_健保用藥品項.csv', "wb").write(response.content)
+def nhi_to_license(nhi_database): #舊方法，用健保碼轉衛署字號，可能會有部分轉錯的問題
     license_convert={'A':'衛署藥製',
-                    'N':'內衛藥製',
-                    'V':'衛署罕藥輸',
                     'B':'衛署藥輸',
-                    'P':'內衛藥輸',
-                    'W':'衛署罕藥製',
                     'J':'衛署菌疫製',
-                    'R':'內衛菌疫製',
-                    'Y':'衛署罕菌疫輸',
                     'K':'衛署菌疫輸',
+                    'N':'內衛藥製',
+                    'P':'內衛藥輸',
+                    'R':'內衛菌疫製',
                     'S':'內衛菌疫輸',
+                    'V':'衛署罕藥輸',
+                    'W':'衛署罕藥製',
+                    'Y':'衛署罕菌疫輸',
                     'Z':'衛署罕菌疫製'}
-    nhi_database=pd.read_csv(r'files/from_tfda/健保用藥品項查詢項目檔_健保用藥品項.csv',low_memory=False)
-    nhi_database=nhi_database.sort_values('有效迄日',ascending=False)
-    nhi_database=nhi_database.drop_duplicates(subset=['藥品代號'],keep='first')
-    nhi_database=nhi_database[['藥品英文名稱', '藥品中文名稱', '成份', 'ATC_CODE', '藥品代號', '單複方', '劑型', '製造廠名稱', '規格量', '規格單位']]
-    nhi_database=nhi_database.fillna('')
     nhi_database['許可證字號']=''
     i=0
     while i<len(nhi_database):
@@ -98,7 +90,59 @@ def update_nhi_database():
         except TypeError:
             nhi_database.iloc[i,10]='專案藥品'
             i+=1
+
+def update_nhi_database():
+    def link_to_license(row):
+        try:
+            license_convert_link={'01':'衛署藥製',
+                                '02':'衛署藥輸',
+                                '03':'衛署成製',
+                                '04':'衛署中藥輸',
+                                '09':'衛署菌疫製',
+                                '10':'衛署菌疫輸',
+                                '12':'內衛藥製',
+                                '13':'內衛藥輸',
+                                '14':'內衛成製',
+                                '15':'內衛菌疫製',
+                                '16':'內衛菌疫輸',
+                                '18':'署藥兼食製',
+                                '19':'衛署成輸',
+                                '20':'衛署罕藥輸',
+                                '21':'衛署罕藥製',
+                                '22':'衛署罕菌疫輸',
+                                '23':'衛署罕菌疫製',
+                                '41':'衛署藥陸輸',
+                                '51':'衛部藥製',
+                                '52':'衛部藥輸',
+                                '53':'衛部成製',
+                                '59':'衛部菌疫製',
+                                '60':'衛部菌疫輸',
+                                '68':'部藥兼食製',
+                                '69':'衛部成輸',
+                                '70':'衛部罕藥輸',
+                                '71':'衛部罕藥製',
+                                '72':'衛部罕菌疫輸',
+                                '73':'衛部罕菌疫製',
+                                '91':'衛部藥陸輸',
+                                '99':'衛署菌製'}
+            if row['藥品代號'][0:1]=='X':
+                row['許可證字號']='專案藥品，無許可證字號'    
+            else:
+                row['許可證字號']=license_convert_link[row['藥品代碼超連結'][-8:-6]]+'字第'+row['藥品代碼超連結'][-6:]+'號'
+        except:
+            row['許可證字號']='許可證字號轉換錯誤'
+        return row
+    #url='https://data.nhi.gov.tw/Datasets/DatasetResource.ashx?rId=A21030000I-E41001-001'
+    url='https://info.nhi.gov.tw/api/iode0000s01/Dataset?rId=A21030000I-E41001-001'
+    response = requests.get(url)
+    open('files/from_tfda/健保用藥品項查詢項目檔_健保用藥品項.csv', "wb").write(response.content)
+    nhi_database=pd.read_csv(r'files/from_tfda/健保用藥品項查詢項目檔_健保用藥品項.csv',low_memory=False)
+    nhi_database=nhi_database.sort_values('有效迄日',ascending=False)
+    nhi_database=nhi_database.drop_duplicates(subset=['藥品代號'],keep='first')
+    #nhi_database=nhi_database[['藥品英文名稱', '藥品中文名稱', '成份', 'ATC_CODE', '藥品代號', '單複方', '劑型', '製造廠名稱', '規格量', '規格單位']]
+    nhi_database=nhi_database.fillna('')
     #nhi_database.to_pickle(r'files\nhi_med.pkl')
+    nhi_database=nhi_database.apply(link_to_license,axis=1)
     df_to_sql(nhi_database)
     #return nhi_database
 
@@ -127,3 +171,6 @@ def download_and_to_sql():
             if os.path.splitext(fullpath)[1]=='.json':
                 json_to_sql(fullpath)
     update_nhi_database()
+
+if __name__=='__main__':
+    download_and_to_sql()
