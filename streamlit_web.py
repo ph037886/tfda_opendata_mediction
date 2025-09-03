ffc68ea94b585e65e07824289c91d0f354106f5c
duncanfun <u101530009@gap.kmu.edu.tw>
2025-09-03 20:32:46 +0800

    修正： 1. 參數錯誤問題

--- streamlit_web.py
+++ streamlit_web.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-def search_event():
-    global final_result_container, option, keyword
+def search_event(keyword):
+    global final_result_container, option
     final_result_container.write(option)
     final_result_container.write(keyword)
     
@@ -10,7 +10,7 @@ def search_event():
 st.set_page_config(page_title='藥品查詢-政府開放資源',layout="wide") #修改網頁title，並預設為寬廣模式
 
 #以下開始功能區
-option = st.selectbox('查詢欄位',('全部欄位''許可證字號','英文品名', '主成分',  '中文品名', '適應症', '外觀', 'ATCCODE', '健保碼'))
+option = st.selectbox('查詢欄位',('全部欄位','許可證字號','英文品名', '主成分',  '中文品名', '適應症', '外觀', 'ATCCODE', '健保碼'))
 keyword=st.text_input('請輸入關鍵字')
 search_button=st.button('搜尋',type="primary")
 st.markdown("""---""")
