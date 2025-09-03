5876cfa72a17a572d084bbf2ab6f756b50841628
duncanfun <u101530009@gap.kmu.edu.tw>
2025-09-03 20:29:55 +0800

    新增： 1. streamlit基礎內容 2. 新增requirements.txt

---
+++ streamlit_web.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+def search_event():
+    global final_result_container, option, keyword
+    final_result_container.write(option)
+    final_result_container.write(keyword)
+    
+
+#以下開始streamlit語法
+st.set_page_config(page_title='藥品查詢-政府開放資源',layout="wide") #修改網頁title，並預設為寬廣模式
+
+#以下開始功能區
+option = st.selectbox('查詢欄位',('全部欄位''許可證字號','英文品名', '主成分',  '中文品名', '適應症', '外觀', 'ATCCODE', '健保碼'))
+keyword=st.text_input('請輸入關鍵字')
+search_button=st.button('搜尋',type="primary")
+st.markdown("""---""")
+final_result_container=st.container()
+search_result_container=st.container()
+
+st.write('網站內容資料來自政府開放平台')
+st.write('Design by 國軍左營總醫院 臨床藥劑科 方志文 藥師')
+
+if keyword:
+    search_event(keyword)
+if search_button:
+    search_event(keyword)
