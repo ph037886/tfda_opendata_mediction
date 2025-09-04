d5295cb32ef868aa39c9d9682b06ada0577e4285
duncanfun <u101530009@gap.kmu.edu.tw>
2025-09-04 21:01:55 +0800

    修正錯誤

--- streamlit_web.py
+++ streamlit_web.py
@@ -24,7 +24,7 @@ def search_event(keyword, option):
 
 #以下開始streamlit語法
 st.set_page_config(page_title='藥品查詢-政府開放資源',layout="wide") #修改網頁title，並預設為寬廣模式
-
+st.markdown('## 藥品查詢-政府開放資源') #用markdown可以讓title變得比較小，比較好看
 #以下開始功能區
 option = st.selectbox('查詢欄位',('全部欄位','許可證字號','英文品名', '主成分',  '中文品名', '適應症', '外觀', 'ATCCODE', '健保碼'))
 keyword=st.text_input('請輸入關鍵字')
--- view_streamlit.py
+++ view_streamlit.py
@@ -4,10 +4,10 @@ import pandas as pd
 def choose_button(column, keyword):
     df=sql_control.pd_read_sql(sql_control.search_medication_by_something(column,keyword))
     df=df.drop_duplicates(subset=['許可證字號'])
-    df=df[df['支付價']!=0]
     return df
 
 def final_result(license_code):
     df=sql_control.pd_read_sql(sql_control.search_for_streamlit(license_code))
+    df=df[df['支付價']!=0]
     df=df.iloc[0]
     return df
