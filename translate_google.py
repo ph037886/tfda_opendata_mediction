f34095604281dd0672df9bb5be22b84982e29977
duncanfun <u101530009@gap.kmu.edu.tw>
2025-09-05 10:26:27 +0800

    修正錯誤

--- translate_google.py
+++ translate_google.py
@@ -1,8 +1,6 @@
-import asyncio
 from googletrans import Translator
 
-async def translate_text(txt):
-    async with Translator() as translator:
-        result = await translator.translate(str(txt), dest='zh')
-        print(result)
-    return result
+def translate_text(txt):
+    translator = Translator()
+    result = translator.translate(str(txt), dest='zh-TW')  # zh-TW = 繁體中文
+    return result.text
