fa506bef10c1a7aec694613a7eaa4f2260a0c1cd
duncanfun <u101530009@gap.kmu.edu.tw>
2025-09-23 19:56:46 +0800

    新增： 1. 新增離線使用的argos功能，待測試

--- translate_argos.py
+++ translate_argos.py
@@ -1,5 +1,6 @@
 import argostranslate.package
 import argostranslate.translate
+import argostranslate
 
 def update_argos():
     
@@ -34,3 +35,33 @@ def trans_to_chinese_by_argos(txt):
     #print(translatedText)
     #print(type(translatedText))
     return translatedText
+
+def dict_argosmodel_path(lang):
+    from_to_argosmodel={'en_ja':'translate-en_ja-1_1.argosmodel',
+                        'en_ko':'translate-en_ko-1_1.argosmodel',
+                        'en_th':'translate-en_th-1_9.argosmodel',
+                        'en_zt':'translate-en_zt-1_9.argosmodel',
+                        'ja_en':'translate-ja_en-1_1.argosmodel',
+                        'ko_en':'translate-ko_en-1_1.argosmodel',
+                        'th_en':'translate-th_en-1_9.argosmodel',
+                        'zt_en':'translate-zt_en-1_9.argosmodel',
+                        'zh_en':'translate-zh_en-1_9.argosmodel'}
+    return from_to_argosmodel[lang]
+
+def trans_with_argos_offline(from_lang, to_lang, text):
+    lang_name_to_code={'正體中文':'zt',
+                       '英文':'en',
+                       '日文':'ja',
+                       '韓文':'ko',
+                       '泰文':'th',
+                       '簡體中文':'zh'}
+    
+    from_code = lang_name_to_code[from_lang]
+    to_code = lang_name_to_code[to_lang]
+    argosmodel_path='files/argosmodel/'
+    argosmodel_path=argosmodel_path + dict_argosmodel_path[from_code+'_'+to_code]
+    
+    argostranslate.package.install_from_path(argosmodel_path)
+    translatedText = argostranslate.translate.translate(text, from_code, to_code)
+    return translatedText
+    
